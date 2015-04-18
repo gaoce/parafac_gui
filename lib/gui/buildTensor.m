b3583559e4757cb8ba51a766fd8992ac6b66d919
gaoce <gaoce@coe.neu.edu>
2015-04-18 16:10:56 -0400

    Add more comments to buildTensor

--- lib/gui/buildTensor.m
+++ lib/gui/buildTensor.m
@@ -5,6 +5,16 @@ function EEMData = buildTensor(fileNames, pathName, numEm, numEx)
 %   numEx: number of excitation wavelengths
 %   numEm: number of emission wavelengths
 
+% Returns
+% A struct with the following fields:
+%   X: a tensor, sample x Em x Ex
+%   Ex: Ex wavelengths
+%   Em: Em wavelengths
+%   nEx: number of Ex
+%   nEm: number of Em
+%   nSample: number of samples
+%   Sample: names of samples
+
 % Num of sample is number of input files
 numSample = length(fileNames);
 
@@ -13,9 +23,15 @@ X = zeros(numSample, numEm, numEx);
 
 Sample = cell(numSample,1);
 for i = 1:numSample
-    fullName = [pathName, '/', fileNames{i}];
-    [~,name,~] = fileparts(fullName); % get file name excluding extension
-    [mat, Ex, Em] = getMat(fullName);
+    % Construct file name
+    fileName = [pathName, '/', fileNames{i}];
+    
+    % Get sample name, namely file name excluding extension
+    [~,name,~] = fileparts(fileName);
+    
+    % Get data
+    [mat, Ex, Em] = getMat(fileName);
+    
     X(i,:,:) = mat;
     Sample{i} = name;
 end
@@ -31,20 +47,21 @@ EEMData.Sample = Sample;
 end
 
 
-function [mat, Ex, Em] = getMat(fullName)
+function [mat, Ex, Em] = getMat(fileName)
 
-fid = fopen(fullName);
-formSpec = ['%s\t', repmat('%f\t',1,47)];
-C = textscan(fid, formSpec, 1, 'CollectOutput', 1, 'HeaderLines',3);
+% Open file
+fid = fopen(fileName);
+
+formSpec = ['%s\t', repmat('%f\t', 1, 47)];
+C = textscan(fid, formSpec, 1, 'CollectOutput', 1, 'HeaderLines', 3);
 Ex = C{2}(:);
 
-formSpec = [repmat('%f\t',1,47), '%f'];
+formSpec = [repmat('%f\t', 1, 47), '%f'];
 C = textscan(fid, formSpec, 'CollectOutput', 1);
 
 Em = C{1}(:,1);
 mat = C{1}(:,2:end);
 
 fclose(fid);
-
 
 end

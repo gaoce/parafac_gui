79c89469b5c5abe86db509847d3230fda0fb2560
gaoce <gaoce@coe.neu.edu>
2015-04-23 18:31:09 -0400

    Comment test.m file

--- test.m
+++ test.m
@@ -1,8 +1,12 @@
+% clear residual objects
 clear;
+
+% Add paths, especially mock functions
 setup('test');
 
+% Construct TestSuite
 import matlab.unittest.TestSuite;
-
 fileSuite = TestSuite.fromFile('GuiTest.m'); 
 
+% Run the test
 result = run(fileSuite);

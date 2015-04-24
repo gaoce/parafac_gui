3675bc8e4190226e6a4029c18d878e39d08940b0
gaoce <gaoce@coe.neu.edu>
2015-04-24 14:21:50 -0400

    Update buildTensor, no longer require numEm and numEx as input

--- src/buildTensor.m
+++ src/buildTensor.m
@@ -1,9 +1,7 @@
-function EEMData = buildTensor(fileNames, pathName, numEm, numEx)
+function EEMData = buildTensor(fileNames, pathName)
 % Arguments:
 %   fileNames: name of input files
 %   pathName: path of input files
-%   numEx: number of excitation wavelengths
-%   numEm: number of emission wavelengths
 
 % Returns
 % A struct with the following fields:
@@ -15,13 +13,24 @@ function EEMData = buildTensor(fileNames, pathName, numEm, numEx)
 %   nSample: number of samples
 %   Sample: names of samples
 
+% Num of header lines and title lines
+% DATATYPE	 3 Dimensional Data            <= header line
+% XYZUNITS	 Nanometer;Nanometer;Emission  <= header line
+% DECIMALSYMBOL	 .                         <= header line
+% X/Y	220.0	225.0	...                <= title line
+nHeader = 3;
+
 % Num of sample is number of input files
 numSample = length(fileNames);
+
+% Get Em and Ex number by reading the first file, they should have the same
+% dimension, or the experiments are wrong. There is a check below
+[numEm, numEx] = getNums(fileNames{1}, pathName);
 
 % Preallocation of tensor
 X = zeros(numSample, numEm, numEx);
 
-Sample = cell(numSample,1);
+sampleNames = cell(numSample,1);
 
 for i = 1:numSample
     % Construct file name
@@ -43,7 +52,7 @@ for i = 1:numSample
     X(i,:,:) = mat;
     
     % Add sample name
-    Sample{i} = name;
+    sampleNames{i} = name;
 end
 
 % Construct EEM obj
@@ -53,36 +62,63 @@ EEMData.Em = Em;
 EEMData.nEx = numEx;
 EEMData.nEm = numEm;
 EEMData.nSample = numSample;
-EEMData.Sample = Sample;
+EEMData.Sample = sampleNames;
 
-end
+    function [numEm, numEx] = getNums(fileName, pathName)
+        % Get the numbers of lines (numEm in the context of this file) and
+        % columns (numEx) in the data table.
+       
+        fd = fopen([pathName, '/', fileName]);
+        
+        % Skip the header lines
+        for iNotUsee = 1:nHeader
+            fgetl(fd);
+        end
+        
+        % Count the number of tabs in title line, numEx = numTab - 1 + 1
+        titleLine = fgetl(fd);
+        numEx = length(strfind(titleLine, sprintf('\t')));
+        
+        % Count the lines in the data table
+        numEm = 0;
+        tline = fgetl(fd);      
+        % Stop in the end or if the trailing lines in the files are blank lines
+        while ischar(tline) && ~isempty(tline)
+            numEm = numEm + 1;
+            tline = fgetl(fd);
+        end
+        
+        fclose(fd);
+    end
 
+    function [mat, Em, Ex] = getIntensityMatrix(fileName, numEx)
+        % The backstage hero: read the file and extract intensity matrix and
+        % wavelengths used in the experiment
+        % Arguments:
+        %   fileName: name of input data
+        %   numEx: number of ex wavelength
+        % Returns:
+        %   mat: intensity matrix
+        %   Em: emission wavelengths
+        %   Ex: exciation wavelengths
+        
+        % Open file
+        fid = fopen(fileName);
+        
+        % Read the fourth line (because the first 3 are header lines)
+        formSpec = ['%s\t', repmat('%f\t', 1, numEx)];
+        C = textscan(fid, formSpec, 1, 'CollectOutput', 1, ...
+            'HeaderLines', nHeader);
+        Ex = C{2}(:); % Excitation wavelengths
+        
+        % Read the remaining lines
+        formSpec = [repmat('%f\t', 1, numEx), '%f'];
+        C = textscan(fid, formSpec, 'CollectOutput', 1);
+        
+        Em = C{1}(:,1); % Emission wavelengths
+        mat = C{1}(:,2:end); % intensity matrix
+        
+        fclose(fid);
+    end
 
-function [mat, Em, Ex] = getIntensityMatrix(fileName, numEx)
-% The backstage hero: read the file and extract intensity matrix and
-% wavelengths used in the experiment
-% Arguments:
-%   fileName: name of input data
-%   numEx: number of ex wavelength
-% Returns:
-%   mat: intensity matrix
-%   Em: emission wavelengths
-%   Ex: exciation wavelengths
-
-% Open file
-fid = fopen(fileName);
-
-% Read the fourth line (because the first 3 are header lines)
-formSpec = ['%s\t', repmat('%f\t', 1, numEx)];
-C = textscan(fid, formSpec, 1, 'CollectOutput', 1, 'HeaderLines', 3);
-Ex = C{2}(:); % Excitation wavelengths
-
-% Read the remaining lines
-formSpec = [repmat('%f\t', 1, numEx), '%f'];
-C = textscan(fid, formSpec, 'CollectOutput', 1);
-
-Em = C{1}(:,1); % Emission wavelengths
-mat = C{1}(:,2:end); % intensity matrix
-
-fclose(fid);
 end

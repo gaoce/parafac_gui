7c0c67a564f3dea6e1e9f035918db041a86ad0f3
gaoce <gaoce@coe.neu.edu>
2015-04-25 00:17:51 -0400

    Update testing setup

--- test.m
+++ test.m
@@ -4,6 +4,11 @@ clear;
 % Add paths, especially mock functions
 setup('test');
 
+% Add results folder to test
+if ~exist('test/results', 'dir')
+    mkdir('test/results');
+end
+
 % Construct TestSuite
 import matlab.unittest.TestSuite;
 testCases = [Callbacks];

1e98d59430194adb68c146ec8aaa3a53538e630c
gaoce <gaoce@coe.neu.edu>
2015-04-24 23:47:20 -0400

    Use persistent instead global in uigetfile

--- test/uigetfile.m
+++ test/uigetfile.m
@@ -1,13 +1,24 @@
 function [filename, pathname, filterindex] = uigetfile(varargin)
 %UIGETFILE a mock function for uigetfile
 
-global FILENAME;
-global PATHNAME;
-global FILTERINDEX;
+persistent FILENAME PATHNAME;
 
-filename = FILENAME;
-pathname = PATHNAME;
-filterindex = FILTERINDEX;
+% If the function is primed?
+flagPrime = 0;
+for i = 1:nargin
+    if strcmp(varargin{i}, 'expOut')
+        FILENAME = varargin{i+1}{1};
+        PATHNAME = varargin{i+1}{2};
+        flagPrime = 1;
+    end
+end
+
+% Expect to work
+if flagPrime == 0
+    filename = FILENAME;
+    pathname = PATHNAME;
+    filterindex = 1;
+end
 
 end
 
